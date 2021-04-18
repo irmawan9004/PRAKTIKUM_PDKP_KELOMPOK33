c6165702ef02d7a19c9e6aa57b7060970c733a23
Muhammad Rizal Qowi <82750972+rizal5516@users.noreply.github.com>
2021-04-18 16:09:36 +0700

    Update Modul4_TugasC++.cpp

--- Modul4_TugasC++/Modul4_TugasC++.cpp
+++ Modul4_TugasC++/Modul4_TugasC++.cpp
@@ -6,7 +6,18 @@ using namespace std;
 int footer(int total);
 
 class nama {
+public:
+    void anggota()
+    {
 
+        cout << "\tKelompok 33 shift 2\n" << endl;
+        cout << "\tMuhammad hafizh zikry 21120120120034" << endl;
+        cout << "\tMuhammad rizal qowi al-azis 21120120140172" << endl;
+        cout << "\tMuhammad irmawan 21120120130125" << endl;
+        cout << "\tNaufal daffa' riswanda' 21120120130107" << endl;
+       
+    }
+};
 int main() {
     
 
